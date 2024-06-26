9df871e07d7bcd43c9a6c6fd83a62ae7fdf117b1
Pawel Kowalski <pawel.kowalski@o2.pl>
2024-06-26 22:04:57 +0200

    stworzenie wykresow

--- projekt_tworzenie.py
+++ projekt_tworzenie.py
@@ -94,3 +94,107 @@ print(data.describe())
 print('head', data.head(5))
 print('tail', data.tail(5))
 print(type(data))
+
+
+# In[10]:
+
+
+# wybór danych z kolumny 'Close'
+series = data.Close.to_numpy()
+#print(series)
+
+
+# In[11]:
+
+
+# Tworzenie wykresu z danych
+plt.plot(data.index, data["Close"], linestyle='-', color='darkblue')  # Wykres linii
+plt.xlabel('Czas')
+plt.ylabel('Wartość [USD]')
+plt.title(f'Wartość zamknięcia dla {tickers[ticker]}')
+plt.grid(color='lightgray')
+plt.tight_layout()
+plt.show()
+
+
+# In[12]:
+
+
+# def. wykres
+def plot_series(time, series_scaled, format="-", start = 0, end = None):
+    plt.plot(time[start:end], series_scaled[start:end], format)
+    plt.xlabel("Czas")
+    plt.ylabel("Wartość")
+    plt.grid(True)
+
+
+# In[13]:
+
+
+# przygotowanie danych do wykresu
+data_1 = data
+data_1['Month'] = data_1.index.month
+data_1['Year'] = data_1.index.year
+
+days = 1825 # ostatnie dni
+days_ses = int(days/7*5) # dni sesyjne
+data_3 = data_1[-days_ses:]
+
+
+# In[14]:
+
+
+# Tworzenie wykresu pudełkowego
+fig, ax1 = plt.subplots(ncols=1, figsize=(12, 8))
+sns.boxplot(data=data_3, x='Month', y='Close', ax=ax1, hue='Month', palette='muted',width=7)
+plt.ylabel('Wartość zamknięcia [USD]')
+plt.xlabel('Miesiące')
+plt.title(f'Wykres pudełkowy dla {tickers[ticker]} z podziałem na miesiące, ostatnie {days} dni')  # Tytuł wykresu
+plt.grid(color='lightgray')
+ax1.get_legend().remove()
+plt.tight_layout()
+plt.show()
+
+
+# In[15]:
+
+
+# przygotowanie danych do dekompozycji + regresja
+result = seasonal_decompose(data.Close[:], model='None', period=720)
+result_trend = result.trend.dropna()
+data_decomp = result_trend.reset_index()
+data_decomp['Date_num'] = mdates.date2num(data_decomp.Date)
+model = LinearRegression()
+X = data_decomp[['Date_num']]
+y = data_decomp['trend']
+model.fit(X, y)
+y_pred = model.predict(X)
+
+
+# In[16]:
+
+
+# przygotowanie danych do dekompozycji + regresja
+result_observed = result.observed.dropna()
+data_obs = result_observed.reset_index()
+data_obs['Date_num'] = mdates.date2num(data_obs.Date)
+model = LinearRegression()
+X1 = data_obs[['Date_num']]
+y1 = data_obs['Close']
+model.fit(X1, y1)
+y_pred_1 = model.predict(X1)
+
+
+# In[17]:
+
+
+#wykres dekompozycji + regresja
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(10, 10), gridspec_kw={'height_ratios': [2, 1 ]})
+ax0.set_title(f'Wykres dla {tickers[ticker]}')
+ax0.plot(result.observed)
+ax0.plot(data_obs['Date'], y_pred_1, color='red')
+ax1.set_title('Trend')
+ax1.plot(result.trend, color='purple')
+plt.grid(color='gray')
+plt.show()
+
